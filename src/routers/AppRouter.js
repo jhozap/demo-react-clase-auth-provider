65075dc395c9967ae380bcdfa3555b0b03c74264
Jhonny <jhonny.z1022@gmail.com>
2021-12-18 09:17:26 -0500

    pruebas AppRouter react

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,12 +3,20 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LoginPage from '../components/login/LoginPage';
 import ContentRoutes from './ContentRoutes';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
+
+                {/* <Route path="/login" element={<LoginPage />} /> */}
 
                 <Route path="/*" element={
                     <PrivateRoute>
@@ -16,7 +24,7 @@ const AppRouter = () => {
                     </PrivateRoute>
                 } />
 
-                <Route path="/*" element={<ContentRoutes />} />
+                {/* <Route path="/*" element={<ContentRoutes />} /> */}
 
             </Routes>
         </BrowserRouter>
